1d79385532c4401a2f62458b0903c907a32aa356
haziqhhussain <hussainhaziqh@gmail.com>
2026-07-17 14:28:23 +0530

    Update and rename 225.Implement Stack using Queues.java to 225.(H.W.)Implement Stack using Queues.java

--- Day 2/225.(H.W.)Implement Stack using Queues.java
+++ Day 2/225.(H.W.)Implement Stack using Queues.java
@@ -1,14 +1,11 @@
 import java.util.LinkedList;
 import java.util.Queue;
-
 class MyStack {
 
     private Queue<Integer> queue;
-
     public MyStack() {
         queue = new LinkedList<>();
     }
-    
     public void push(int x) {
         queue.add(x);
         int size = queue.size();
@@ -16,15 +13,12 @@ class MyStack {
             queue.add(queue.remove());
         }
     }
-    
     public int pop() {
         return queue.remove();
     }
-    
     public int top() {
         return queue.peek();
     }
-    
     public boolean empty() {
         return queue.isEmpty();
     }
