c0f1633c68a5a12bde866defe84c0034477270a0
haziqhhussain <hussainhaziqh@gmail.com>
2026-07-17 14:13:16 +0530

    Update 14.shuffle the array.java

--- Day 2/14.shuffle the array.java
+++ Day 2/14.shuffle the array.java
@@ -1,17 +1,10 @@
 class Solution {
     public int[] shuffle(int[] nums, int n) {
-        // Create a new result array of size 2n
         int[] result = new int[2 * n];
-        
-        // Use an index to track where we place elements in the result array
-        int index = 0;
-        
-        // Loop through the first half of the array
+        int index = 0;        
         for (int i = 0; i < n; i++) {
-            result[index++] = nums[i];     // Append x_i
-            result[index++] = nums[i + n]; // Append y_i
-        }
-        
+            result[index++] = nums[i];     
+            result[index++] = nums[i + n];         
         return result;
     }
 }
