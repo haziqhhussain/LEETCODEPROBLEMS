3e29a4dd119a3ca0f94c1cfba800fcbaffb00843
haziqhhussain <hussainhaziqh@gmail.com>
2026-07-19 15:15:23 +0530

    Remove comments from twoSum method
    Removed comments to clean up the code.

--- Day 2/1.Two sum.java
+++ Day 2/1.Two sum.java
@@ -1,10 +1,7 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        // Look at every number
         for (int i = 0; i < nums.length; i++) {
-            // Look at every number after it
             for (int j = i + 1; j < nums.length; j++) {
-                // If they add up to the target, return their positions
                 if (nums[i] + nums[j] == target) {
                     return new int[] { i, j };
                 }
