5613a0783924b00aec17f672d2364dca782e80f2
haziqhhussain <hussainhaziqh@gmail.com>
2026-07-21 14:16:33 +0530

    Clean up comments in Valid Palindrome implementation
    Removed comments to clean up the code.

--- Day 7/125.(H.W.)Valid Palindrome.java
+++ Day 7/125.(H.W.)Valid Palindrome.java
@@ -4,24 +4,18 @@ class Solution {
         int right = s.length() - 1;
         
         while (left < right) {
-            // Move left pointer rightward until an alphanumeric character is found
             while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
                 left++;
             }
-            // Move right pointer leftward until an alphanumeric character is found
             while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
                 right--;
             }
-            
-            // Compare characters ignoring case
             if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
                 return false;
             }
-            
             left++;
             right--;
         }
-        
         return true;
     }
 }
