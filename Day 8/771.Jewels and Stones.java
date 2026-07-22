6d585f1d5ec5513a6fbef04cc800ac6084be6284
haziqhhussain <hussainhaziqh@gmail.com>
2026-07-22 13:49:40 +0530

    Add Solution class for Jewels and Stones problem
    Implement a method to count jewels in stones.

---
+++ Day 8/771.Jewels and Stones.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+        boolean[] isJewel = new boolean[128];
+        for (char j : jewels.toCharArray()) {
+            isJewel[j] = true;
+        }
+        int count = 0;
+        for (char s : stones.toCharArray()) {
+            if (isJewel[s]) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
